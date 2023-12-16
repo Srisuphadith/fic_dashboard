4532c50bf0c28190b6021724eaccf763c33897e2
FiFaKuBz <fifa.yanakorn@gmail.com>
2023-12-16 21:34:24 +0700

    FIC

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $s = "localhost";
 $u = "root";
 $p = "12345678";
-$d = "fic";
+$d = "FIC";
 
 $conn = mysqli_connect($s,$u,$p,$d) or die("Connection Fail".mysqli_connect_error());
 mysqli_set_charset($conn,"utf8");
