62d15d9c66877656efafb58487537227db74cf70
Nine <ninerattana2005@gmail.com>
2023-12-17 13:18:43 +0700

    add status debug

--- status_debug.php
+++ status_debug.php
@@ -3,27 +3,26 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>sensor data debug</title>
+    <title>Status Debug</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
 <body>
 <?php
 require_once("connect.php");
-$sql = "SELECT * FROM Sensor_data ORDER BY id DESC";
+$sql = "SELECT * FROM status ORDER BY id DESC";
 $result = mysqli_query($conn,$sql);
 ?>
 <table class="table">
     <tr>
         <th>id</th>
-        <th>Temperature</th>
-        <th>Humidity</th>
-        <th>Soil_humidity</th>
+        <th>pump</th>
+        <th>fan</th>
         <th>time_stamp</th>
     </tr>
 <?php
 while($row = mysqli_fetch_assoc($result)){
     echo "<tr>";
-    echo "<td>".$row["id"]."</td><td>".$row["Temperature"]."</td><td>".$row["Humidity"]."</td><td>".$row["Soil_humidity"]."</td><td>".$row["time_stamp"]."</td>";
+    echo "<td>".$row["id"]."</td><td>".$row["pump"]."</td><td>".$row["fan"]."</td><td>".$row["time_stamp"]."</td>";
     echo "</tr>";
 }
 
