142c931bf59cc49ab497ae7c03cec7d9fd6df190
Nine <ninerattana2005@gmail.com>
2023-12-18 09:02:44 +0700

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$s = "localhost";
+$u = "root";
+$p = "12345678";
+$d = "FIC";
+
+$conn = mysqli_connect($s,$u,$p,$d) or die("Connection Fail".mysqli_connect_error());
+mysqli_set_charset($conn,"utf8");
+?>
