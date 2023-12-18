3db22236b56cd679af57f4908fbcf909c76e9586
FiFaKuBz <fifa.yanakorn@gmail.com>
2023-12-19 02:21:16 +0700

    noti with Time Finished viewer only Data Analysis left

--- update_status.php
+++ update_status.php
@@ -5,7 +5,7 @@ $sqlD = "SELECT `pump`, `fan` FROM `status` WHERE `id` = 1";
 $resultD = mysqli_query($conn, $sqlD);
 $deviceStatus = mysqli_fetch_assoc($resultD);
 
-$sqlS = "SELECT `Temperature`, `Humidity`, `Soil_humidity` FROM `Sensor_data` ORDER BY id DESC LIMIT 1";
+$sqlS = "SELECT `time_stamp`,`Temperature`, `Humidity`, `Soil_humidity` FROM `Sensor_data` ORDER BY id DESC LIMIT 1";
 $resultS = mysqli_query($conn, $sqlS);
 $sensorData = mysqli_fetch_assoc($resultS);
 
@@ -14,7 +14,8 @@ $statusData = [
     'fan' => $deviceStatus['fan'],
     'Temperature' => $sensorData['Temperature'],
     'Humidity' => $sensorData['Humidity'],
-    'Soil_humidity' => $sensorData['Soil_humidity']
+    'Soil_humidity' => $sensorData['Soil_humidity'],
+    'time_stamp' => $sensorData['time_stamp']
 ];
 
 header('Content-Type: application/json');
