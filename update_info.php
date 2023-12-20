e726565d5ea9c533ca7992f18eef7d9865bb6cda
FiFaKuBz <fifa.yanakorn@gmail.com>
2023-12-20 18:19:28 +0700

    update session

--- update_info.php
+++ update_info.php
@@ -6,13 +6,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['field']) && isset($_POST['value']) && isset($_SESSION['id'])) {
         $field = $_POST['field'];
         $value = $_POST['value'];
-        $userId = $_SESSION['id'];
+        $userId = $_SESSION['id'];        
+        
+        if($_POST['field'] == 'fname'){
+        $_SESSION['name'] =  $value;
+        }
 
         $field = mysqli_real_escape_string($conn, $field);
         $value = mysqli_real_escape_string($conn, $value);
 
+
+
         $sql = "UPDATE user_info SET $field = '$value' WHERE ID = '$userId'";
         $result = mysqli_query($conn, $sql);
+
+
 
         echo $result ? "Success" : "Error";
     } else {
