a70206c31a4061b751d898b7fb90155c9af58220
Nine <ninerattana2005@gmail.com>
2023-12-20 13:26:21 +0700

    add chart

--- PDO.php
+++ PDO.php
@@ -3,7 +3,7 @@
     // Replace your-hostname, your-db, your-username, your-password according to your database
     $link = new \PDO(   'mysql:host=localhost;dbname=FIC;charset=utf8mb4', //'mysql:host=localhost;dbname=canvasjs_db;charset=utf8mb4',
                         'root', //'root',
-                        '', //'',
+                        '12345678', //'',
                         array(
                             \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                             \PDO::ATTR_PERSISTENT => false
