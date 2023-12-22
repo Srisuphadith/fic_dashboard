c5a8b1daf123d11da71e8b1af37046d721671db4
Nine <ninerattana2005@gmail.com>
2023-12-22 08:27:40 +0700

    wewfw

--- footer.php
+++ footer.php
@@ -44,7 +44,7 @@
         <div class="footerCol">
             <h2 class="head">Contact Us</h2>
             <div class="footerContent">
-            <a herf="mailto:Bit6-CPE101@kmutt.ac.th">Bit6-CPE101@kmutt.ac.th</a>
+            <a href="mailto:Bit6-CPE101@kmutt.ac.th">Bit6-CPE101@kmutt.ac.th</a>
             </div>
         </div>
     </div>
